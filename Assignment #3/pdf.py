1da8534dd8ed1d322769b83ffb712b3300f76ff6
Bruschini Davide <bruschinidavide@bruschinidavide-Lenovo-IdeaPad-Y510P.localdomain>
2019-10-16 10:47:55 +0200

    Added the possibility to change the order of the spline

--- Assignment #3/pdf.py
+++ Assignment #3/pdf.py
@@ -10,10 +10,10 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
     """Class describing a probability density function.
     """
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, k=3):
         """Constructor.
         """
-        InterpolatedUnivariateSpline.__init__(self, x, y)
+        InterpolatedUnivariateSpline.__init__(self, x, y, None, [None]*2, k)
         ycdf = np.array([self.integral(min(x), xcdf) for xcdf in x])
         self.cdf = InterpolatedUnivariateSpline(x, ycdf)
         mask=diff(ycdf) > 0.0
@@ -29,7 +29,7 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
         return self.cdf(x2) - self.cdf(x1)
 
     def rnd(self, size=1000):
-        """Return an array of random values from the pdf.6 
+        """Return an array of random values from the pdf.
         """
         return self.ppf(np.random.uniform(size=size))
     
@@ -37,19 +37,21 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
 
 
 if __name__ == '__main__':
-    x = np.linspace(-3., 3., 101)
+    x = np.linspace(-3., 3., 40)
     y = 1./np.sqrt(np.pi)*np.exp(-(x**2))
     pdf = ProbabilityDensityFunction(x, y)
     a = np.array([0.2, 0.6])
     print(pdf(a))
 
+    array_=np.linspace(-3.,3.,500)
+
     plt.figure('pdf')
-    plt.plot(x, pdf(x))
+    plt.plot(array_, pdf(array_))
     plt.xlabel('x')
     plt.ylabel('pdf(x)')
 
     plt.figure('cdf')
-    plt.plot(x, pdf.cdf(x))
+    plt.plot(array_, pdf.cdf(array_))
     plt.xlabel('x')
     plt.ylabel('cdf(x)')
 
@@ -61,6 +63,33 @@ if __name__ == '__main__':
     plt.ylabel('ppf(x)')
 
     plt.figure('Sampling')
+    rnd = pdf.rnd(1000000)
+    plt.hist(rnd, bins=200)
+    
+    print(pdf.prob(0.2,0.8))
+
+    pdf = ProbabilityDensityFunction(x, y, 1)
+    a = np.array([0.2, 0.6])
+    print(pdf(a))
+
+    plt.figure('pdf2')
+    plt.plot(array_, pdf(array_))
+    plt.xlabel('x')
+    plt.ylabel('pdf(x)')
+
+    plt.figure('cdf2')
+    plt.plot(array_, pdf.cdf(array_))
+    plt.xlabel('x')
+    plt.ylabel('cdf(x)')
+
+
+    z=np.linspace(0.,1.,300)
+    plt.figure('ppf2')
+    plt.plot(z, pdf.ppf(z))
+    plt.xlabel('x')
+    plt.ylabel('ppf(x)')
+
+    plt.figure('Sampling2')
     rnd = pdf.rnd(1000000)
     plt.hist(rnd, bins=200)
     
